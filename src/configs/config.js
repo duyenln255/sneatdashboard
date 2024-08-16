6671fb61b98aa5ade3055aa664d0b0325dab77f0
Duyên Lê <89249440+duyenln255@users.noreply.github.com>
2024-08-16 11:01:09 +0700

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,6 +1,5 @@
 const config = {
     apiUrl: "https://account.agridential.vn/api/v1", // Cấu hình trực tiếp API URL
-    recaptchaSiteKey: "6LcCggwqAAAAADjXZid7-iGjNz0MO2BRmw9-6rgV" // Cấu hình trực tiếp reCAPTCHA site key
   };
   
   export default config;
